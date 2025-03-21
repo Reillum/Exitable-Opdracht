4ba8430b9bbce4e19776fac02c46ecdca7c52f7c
Justin Dang <justindang03@gmail.com>
2025-03-21 16:46:27 +0100

    Added scores per frame for each player. Added statements for the winner or if it's tied.

--- functions.php
+++ functions.php
@@ -1,31 +1,36 @@
 <?php
+require_once "test.php";
 
 function calculateBowlingScore($rolls){
     $score = 0;
     $frame = 0;
     $rollIndex = 0;
+    $frameScore = [];
 
     //Itereren door alle worpen voor elke frame
     while ($frame < 10){
         if (isStrike($rolls, $rollIndex)){
             //Strike: 10 punten + volgende 2 worpen 
             $score += 10 + strikeBonus($rolls, $rollIndex);
+            $frameScore[] = $score;
             $rollIndex++; //Naar de volgende worp gaan
         }
 
         elseif(isSpare($rolls, $rollIndex)){
             //Spare: 10 punten + de volgende worp
             $score += 10 + spareBonus($rolls, $rollIndex);
+            $frameScore[] = $score;
             $rollIndex += 2;
         }
 
         else{
             $score += sumOfTwoRolls($rolls, $rollIndex);
+            $frameScore[] = $score;
             $rollIndex += 2;
         }
         $frame ++;
     }
-    return $score;
+    return $frameScore;
 }
 
 function isStrike($rolls, $rollIndex){
@@ -49,6 +54,19 @@ function sumOfTwoRolls($rolls, $rollIndex){
 
 }
 
+function determineWinner($player_1_score, $player_2_score){
+    if ($player_1_score == $player_2_score){
+        echo "Het is gelijkspel!";
+    }
+    
+    elseif ($player_1_score > $player_2_score){
+        echo "Speler 1 wint!";
+    }
 
+    else{
+        echo "Speler 2 wint!";
+    }
+
+}
 
 ?>
--- test.php
+++ test.php
@@ -2,17 +2,25 @@
 require_once "functions.php";
 
 $player_1_rolls = [10, 7, 3, 9, 1, 10, 10, 10, 2, 3, 6, 4, 10, 7, 2, 6, 4, 9, 1];
+
 $player_2_rolls = [9, 0, 8, 2, 7, 3, 10, 5, 4, 9, 1, 8, 2, 7, 3, 10, 6, 4, 5];
 
 $player_1_score = calculateBowlingScore($player_1_rolls);
 $player_2_score = calculateBowlingScore($player_2_rolls);
 
-// print_r ($player_1_rolls)."<br>";
-// echo "<br>";
-// print_r ($player_2_rolls)."<br>";
-// echo "<br>";
-echo "Totale Score Speler 1: ". $player_1_score. "<br>";
-echo "Totale Score Speler 2: ".$player_2_score. "<br>";
+echo "<h2>Bowling Score Tracker</h2>";
 
+echo "<h3>Score Speler 1:</h3>";
+for ($i = 0; $i < count($player_1_score); $i++) {
+    echo "Frame " . ($i + 1) . ": " . $player_1_score[$i] . "<br>";
+}
 
+echo "<h3>Score Speler 2:</h3>";
+for ($i = 0; $i < count($player_2_score); $i++) {
+    echo "Frame " . ($i + 1) . ": " . $player_2_score[$i] . "<br>";
+}
+
+echo "<br>";
+
+determineWinner($player_1_score,$player_2_score);
 ?>
